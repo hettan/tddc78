b3fb99316a02a8711e54e7f28e5f4e0492196f2b
Jacob <jacobholm89@gmail.com>
2015-04-13 11:57:36 +0200

    some testing still

--- image_filter/mpi/blurmain.c
+++ image_filter/mpi/blurmain.c
@@ -1,29 +1,67 @@
 #include <mpi.h>
+#include <iostream>
 
-typedef struct _pixel 
+struct pixel
 {
-  usigned char r,g,b;
-} pixel;
-  
+  unsigned char r,g,b;
+};
+
+using namespace std;
+
+namespace std{
+  ostream& operator<<(ostream& os, pixel& p){
+    os << "r=" << p.r << ", g=" << p.g << ", b=" << p.b;
+    return os;
+  }
+} 
+
 int main(int argc, char** argv)
 {
-  MPI_Status status;
-  char *string = "xxxxx"; //rcv buff
+  MPI::Status status;
   int myid;
+  MPI::Init( argc, argv );
+  myid = MPI::COMM_WORLD.Get_rank();
+  cout << "My id is " << myid << endl;
 
-  MPI_Init( NULL, NULL );
-  MPI_Comm_rank( MPI_COMM_WORLD, &myid);
-  
-  if(myid==2)
-    MPI_Send( "Hello", 5, MPI_CHAR, 7, 1234, MPI_COMM_WORLD );
-  
-  if(myid==7) {
-    MPI_Recv( string, 5, MPI_CHAR, 2, MPI_ANY_TAG,
-	      MPU_COMM_WORLD, &status);
-    printf( "Got %s from P&d, tag %d\n",
-	    string, status.MPI_SOURCE, status.MPI_TAG);
+  int size = 3;
+  int blockcounts[] = {1, 1, 1};
+  MPI_Aint disp[] = {0, 0, 0};
+  MPI_Datatype old_types[3] = {MPI::UNSIGNED_CHAR, MPI::UNSIGNED_CHAR, MPI::UNSIGNED_CHAR};
+
+  MPI_Datatype pixel_type;
+  MPI_Type_struct(size, blockcounts, disp, old_types, &pixel_type);
+  MPI_Type_commit(&pixel_type);
+
+  char buf[5];
+
+  pixel test;
+
+  int tag = 1;
+  int rc;
+  if(myid==0){
+    int dst = 1;
+    int src = 1;
+    
+    test.r = 'a';
+    test.g = 'b';
+    test.b = 'c';
+    MPI::COMM_WORLD.Send( &test, 3, pixel_type, dst, tag );
+    //string msg = "Hello";
+    //MPI::COMM_WORLD.Send( msg.c_str(), 5, MPI::CHAR, dst, tag );
+    //MPI::COMM_WORLD.Recv( buf, 5, MPI::CHAR, src, tag, status);
+    //cout << "Got " << buf << " from " << src << endl;
   }
-  MPI_Finalize();
+  if(myid==1) {
+    int src = 0;
+    int dst = 0;
+    
+    MPI::COMM_WORLD.Recv( &test, 3, pixel_type, src, tag);
+    cout << test << endl;
+    /*
+    MPI::COMM_WORLD.Recv( buf, 5, MPI::CHAR, src, tag, status);
+    cout << "Got " << buf << " from " << src << endl;
+    MPI::COMM_WORLD.Send( buf, 5, MPI::CHAR, dst, tag ); */
   }
+  MPI::Finalize();
   return 0;
 }
---
+++ image_filter/mpi/blurmain.cc
@@ -0,0 +1,89 @@
+#include <mpi.h>
+#include <iostream>
+#include <math.h>
+
+#define MAX_X 1.33
+#define Pi 3.14159
+
+
+struct pixel
+{
+  unsigned char r,g,b;
+};
+
+using namespace std;
+
+namespace std{
+  ostream& operator<<(ostream& os, pixel& p){
+    os << "r=" << p.r << ", g=" << p.g << ", b=" << p.b;
+    return os;
+  }
+}
+
+
+void get_gauss_weights(int i, int n, double* weights_out)
+{
+  double x;
+  for(; i<n; i++ ){
+    x=(double)i * MAX_X/n;
+    weights_out[i] = exp(-x*x * Pi);
+  }
+}
+ 
+
+void init_pixel_type(MPI_Datatype& pixel_type)
+{
+  const int struct_size = 3;
+  int blockcounts[] = {1, 1, 1};
+  MPI_Aint disp[] = {0, 0, 0};
+  MPI_Datatype old_types[struct_size] = {MPI::UNSIGNED_CHAR,
+					 MPI::UNSIGNED_CHAR,
+					 MPI::UNSIGNED_CHAR};
+
+  MPI_Type_struct(struct_size, blockcounts, disp, old_types, &pixel_type);
+  MPI_Type_commit(&pixel_type);
+}
+
+int main(int argc, char** argv)
+{
+  MPI::Status status;
+  MPI::Init( argc, argv );
+  int myid = MPI::COMM_WORLD.Get_rank();
+  int size = MPI::COMM_WORLD.Get_size();
+  
+  MPI_Datatype pixel_type;
+  init_pixel_type(pixel_type);
+
+  pixel test[3];
+
+  int tag = 1;
+  int rc;
+  if(myid==0){
+    cout << "NUMBER OF THREADS = " << size << endl;
+    int dst = 1;
+    int src = 1;
+    
+    test[0] = pixel{'a','b','c'};
+    test[1] = pixel{'d','e','f'};
+    test[2] = pixel{'g','h','i'};
+    MPI::COMM_WORLD.Send( &test[0], 3, pixel_type, dst, tag );
+    
+  }
+  else if(myid==1) {
+    int src = 0;
+    int dst = 0;
+    
+    MPI::COMM_WORLD.Recv( &test[0], 3, pixel_type, src, tag);
+    cout << test[0] << endl;
+  }
+  int root=0; //main process
+
+  const int work_size = size*10;
+  
+  double *work = new double [work_size*sizeof(double)];
+  MPI::COMM_WORLD.Scatter( work, work_size/size, MPI::DOUBLE,
+			   rcv, work_size/size, root);
+  
+  MPI::Finalize();
+  return 0;
+}
