4209dd239b1ba413d12f6686fd25c170c270ef20
hettan <jacobholm89@gmail.com>
2015-04-13 09:13:34 +0200

    test

--- image_filter/mpi/blurmain.c
+++ image_filter/mpi/blurmain.c
@@ -1,11 +1,29 @@
 #include <mpi.h>
 
-typedef struct _pixel {
+typedef struct _pixel 
+{
   usigned char r,g,b;
 } pixel;
   
-int main(int arc, char** argv)
+int main(int argc, char** argv)
 {
+  MPI_Status status;
+  char *string = "xxxxx"; //rcv buff
+  int myid;
 
-
+  MPI_Init( NULL, NULL );
+  MPI_Comm_rank( MPI_COMM_WORLD, &myid);
+  
+  if(myid==2)
+    MPI_Send( "Hello", 5, MPI_CHAR, 7, 1234, MPI_COMM_WORLD );
+  
+  if(myid==7) {
+    MPI_Recv( string, 5, MPI_CHAR, 2, MPI_ANY_TAG,
+	      MPU_COMM_WORLD, &status);
+    printf( "Got %s from P&d, tag %d\n",
+	    string, status.MPI_SOURCE, status.MPI_TAG);
+  }
+  MPI_Finalize();
+  }
+  return 0;
 }
